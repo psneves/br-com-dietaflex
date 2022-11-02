8220b726ccf2c93b9573d27b4055d6f2ad0e4b44
psneves <paulo@psneves.com.br>
2022-11-02 18:56:30 -0300

    Added django project

---
+++ app/bouncer/urls.py
@@ -0,0 +1,14 @@
+"""
+URL Mapping for bouncer.
+"""
+from django.urls import path
+
+from bouncer import views
+
+
+app_name = 'bouncer'
+
+
+urlpatterns = [
+    path('',views.landing, name='landing')
+]
